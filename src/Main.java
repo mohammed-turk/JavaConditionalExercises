2e0e7a6f72437dbfdc3176cc299bab776900a5f5
mohammed-rurk <mtj6611@gmail.com>
2026-08-11 21:29:54 +0300

    update: removing unnesseccary comment

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
